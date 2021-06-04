7d556f5280699e9f5fb428bd7f2087d2912b960e
abhilash <abhilash.kamble376@gmail.com>
2021-06-04 11:48:23 +0530

    router

--- day-20/app1/src/App.js
+++ day-20/app1/src/App.js
@@ -1,13 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-// import CounterApp from './components/counter-Class'
+import About from './components/about'
+import Product from './components/products'
 
-import CounterApp from './components/Counter-func'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <CounterApp />
+      <h1>My application</h1>
+
+      <Router>
+
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/product">Product</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/about" component={About}></Route>
+          <Route path="/product" component={Product}></Route>
+        </Switch>
+
+
+
+      </Router>
+
+
+
 
     </div >
   );
