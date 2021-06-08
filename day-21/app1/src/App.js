a56751ca5564f098c876ec7d1ce7aeca87d539ab
abhilash <abhilash.kamble376@gmail.com>
2021-06-08 11:12:43 +0530

    update

--- day-21/app1/src/App.js
+++ day-21/app1/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-
+      
       <Router>
 
         <Link to="/header"> Header </Link>
